b96e07c84000396464d2605cbab29acba514e5f1
RySes <rksesgundo123@gmail.com>
2022-05-26 11:50:03 +0800

    Create secondprog_multiplication_table.cs

---
+++ secondprog_multiplication_table.cs
@@ -0,0 +1,20 @@
+using System;
+
+class Program{
+	static void Main(String[] args){
+		string a, b;
+		Console.WriteLine("Multiplication Table");
+		Console.Write("Enter number (Vertical): ");
+		a = Console.ReadLine();
+		Console.Write("Enter number (Horizontal): ");
+		b = Console.ReadLine();
+		int x = int.Parse(a);
+		int y = int.Parse(b);
+		for(int i = 1; i <= x; i++){
+			for(int j = 1; j <= y; j++){
+				Console.Write((i * j) + " ");
+			}
+			Console.WriteLine();
+		}
+	}
+}
